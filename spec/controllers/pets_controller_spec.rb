9ac576b1587eeba2eabe6e8a2344d38326d880a8
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 02:12:30 +0000

    Done.

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -13,7 +13,7 @@ describe "Pets Controller" do
       expect(page).to have_field('pet_name')
     end
 
-    it "has a form with a radio buttons for existing owners" do
+    it "has a form with a checkbox for existing owners" do
       @owner1 = Owner.create(:name => "Cricky")
       @owner2 = Owner.create(:name => "Chris")
 
@@ -33,7 +33,7 @@ describe "Pets Controller" do
       @owner2 = Owner.create(:name => "Chris")
       visit '/pets/new'
       fill_in "pet_name", :with => "Michael"
-      choose(@owner1.id)
+      check(@owner1.id)
       click_button "Create Pet"
       @pet = Pet.last
       expect(@pet.name).to eq("Michael")
@@ -56,7 +56,7 @@ describe "Pets Controller" do
       @owner2 = Owner.create(:name => "Kaitlin")
       visit '/pets/new'
       fill_in "pet_name", :with => "Joeseph"
-      choose(@owner2.id)
+      check(@owner2.id)
       click_button "Create Pet"
       @pet= Pet.last
       expect(page.current_path).to eq("/pets/#{@pet.id}")
@@ -69,8 +69,8 @@ describe "Pets Controller" do
       @pet = Pet.create(:name => "Chewie", :owner_id => @owner.id)
     end
 
-    it "can visit '/pets/:id/edit' " do
-      get "/pets/#{@pet.id}/edit"
+    it "can visit '/owners/:id/edit' " do
+      get "/owners/#{@owner.id}/edit"
       expect(last_response.status).to eq(200)
     end
 
@@ -91,7 +91,7 @@ describe "Pets Controller" do
     it "edit's the pet's owner with an existing owner" do
       @adam = Owner.create(:name => "Adam")
       visit "/pets/#{@pet.id}/edit"
-      choose(@adam.id)
+      check(@adam.id)
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Adam")
     end
